16cf5067c0849116e3e60677f76d639e5f2aa64e
KingLichSlagaemie <102833847+KingLichSlagaemie@users.noreply.github.com>
2025-11-25 15:00:27 +0300

    Add files via upload

---
+++ Lab-5/Lab-5.cs
@@ -0,0 +1,469 @@
+﻿using System.Threading.Channels;
+using System.Windows.Markup;
+
+namespace zhadaev_lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //void SayHello()
+            //{
+            //    Console.WriteLine("hello");
+            //}
+            //SayHello();
+
+            //void SayHello1() => Console.WriteLine("Hello");
+            //SayHello1();
+
+
+            //void Game()
+            //{
+            //    string mygame = "The Witcher";
+            //    Console.WriteLine(mygame);
+            //}
+            //Game();
+
+
+            //Console.Write("Введите свое имя: ");
+            //string? name = Console.ReadLine();
+            //Console.Write("Введите ваш язык (русский, немецкий, английский): ");
+            //string language = Console.ReadLine()!.ToLower();
+
+            //void SayHelloRu() => Console.WriteLine($"Привет {name}!");
+            //void SayHelloEn() => Console.WriteLine($"Hello {name}!");
+            //void SayHelloDe() => Console.WriteLine($"Guten Tag{name}");
+
+            //switch (language)
+            //{
+            //    case "английский": SayHelloEn(); break;
+            //    case "русский": SayHelloRu(); break;
+            //    case "немецкий": SayHelloDe(); break;
+            //    default: Console.WriteLine("Язык не найден!"); break;
+            //}
+
+
+
+            //void Print(string message)
+            //{
+            //    Console.WriteLine(message);
+            //}
+            //Print(message: "Hello");
+
+            //Console.WriteLine("What is your name?");
+            //Console.WriteLine("Enter your name: ");
+            //string? name = Console.ReadLine();
+            //Print(message: $"Your name : {name}");
+
+
+            //void Sum(int x, int y)
+            //{
+            //    int result = x + y;
+            //    Console.WriteLine($"{x} + {y} = {result}");
+            //}
+            //Sum(10, 20);
+
+
+            //Console.Write("Введите переменную х");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Введите переменную у");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //Sum(x, y);
+
+            //void PrintPerson(string personName, int age)
+            //{
+            //    Console.WriteLine($"Name: {personName}, Age: {age}");
+            //}
+
+            //Console.WriteLine("Enter your name");
+            //string personName = Console.ReadLine();
+            //Console.WriteLine("Enter your age");
+            //int age = Convert.ToInt32(Console.ReadLine());
+
+            //PrintPerson(personName, age);
+
+
+            //void Person(string name, int age = 18, string vuz = "ВолГУ") =>
+            //    Console.WriteLine($"Name: {name}\nAge: {age}\nCollage: {vuz}");
+            //Person(name: "Alexander");
+            //Person(name: "Ivan", age:30);
+            //Person(name: "Dmitriy", age:20, vuz:"MGU");
+            //Person(age: 30, name: "Ivan");
+            //Person(vuz: "MGU", age: 20, name: "Dmitriy");
+
+
+            //string GetMessage()
+            //{
+            //    return "Hello";
+            //}
+            //Console.WriteLine(GetMessage());
+
+
+            //int Sum(int a, int b)
+            //{
+            //    return a + b;
+            //}
+            //Sum(10, 20);
+
+
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //void CheckVelue(int number)
+            //{
+            //    if (number < 0)
+            //    {
+            //        Console.WriteLine("Отрицательное число");
+            //        return;
+            //    }
+            //    Console.WriteLine("Положительное число");
+            //}
+            //CheckVelue(number);
+
+
+            //string GetMessage() 
+            //{
+            //    return "Hello";
+            //}
+            //string message = GetMessage();
+            //Console.WriteLine(message);
+
+
+            //string GetMessage()
+            //{
+            //    return "Hello";
+            //}
+            //void PrintMessage(string message)
+            //{
+            //    Console.WriteLine(message);
+            //}
+            //PrintMessage(GetMessage());
+
+
+            //string GetMessage()
+            //{
+            //    return "hello";
+            //}
+            //int Sum(int x, int y)
+            //{
+            //    return x + y;
+            //}
+
+            //string GetMessage() => "hello";
+            //int Sum(int x, int y) => x + y;
+
+
+            //void PrintPerson(string name, int age)
+            //{
+            //    if (age > 120 || age < 1)
+            //    {
+            //        Console.WriteLine("Недопустимый возраст");
+            //        return;
+            //    }
+            //    Console.WriteLine($"Имя: {name} Возраст: {age}");
+            //}
+            //PrintPerson(name: "Alex", age: 32);
+            //PrintPerson(name: "Bob", age: 148);
+
+
+            //Random random = new Random();
+            //int number = random.Next(0, 100);
+            //Console.WriteLine($"Случайное число: {number}");
+            //double randomDouble = random.NextDouble();
+            //Console.WriteLine($"Случайное число: {randomDouble}");
+
+
+
+            //void ShowWelcomeMessage() => Console.WriteLine("Прогоз погоды");
+            //void ShowGoodbyeMessage() => Console.WriteLine("Спасибо");
+            //string GetWeather() 
+            //{
+            //    string[] weathers = ["Солнечно", "Облачно", "Дождь", "Снег", "Гроза"];
+            //    Random random = new Random();
+            //    int index = random.Next(weathers.Length);
+            //    return weathers[index];
+            //}
+            //ShowWelcomeMessage();
+            //string currentWeather = GetWeather();
+            //Console.WriteLine($"Погода на сегодня: {currentWeather}");
+            //ShowGoodbyeMessage();
+
+
+            //int Factorial(int n)
+            //{
+            //    if (n == 1) return 1;
+            //    return n * Factorial(n - 1);
+            //}
+
+
+            //int Factorial(int n)
+            //{
+            //    if (n == 1) return 1;
+            //    return n * Factorial(n: n - 1);
+            //}
+
+            //int factorial = Factorial(n: 4);
+            //int factorial1 = Factorial(n: 5);
+            //int factorial2 = Factorial(n: 6);
+
+            //Console.WriteLine($"4 = {factorial}");
+            //Console.WriteLine($"5 = {factorial1}");
+            //Console.WriteLine($"6 = {factorial2}");
+
+
+
+            //void Compare(int[] numbers1, int[] numbers2)
+            //{
+            //    int numbers1Sum = 0;
+            //    int numbers2Sum = 0;
+            //    foreach (int number in numbers1)
+            //        numbers1Sum += number;
+            //    foreach (int number in numbers2) 
+            //        numbers2Sum += number;
+            //    if (numbers1Sum > numbers2Sum)
+            //        Console.WriteLine("Сумма чисел нам1 больше");
+            //    else if (numbers1Sum < numbers2Sum)
+            //        Console.WriteLine("Сумма чисел нам2 больше");
+            //    else
+            //        Console.WriteLine("Сумма маасивов равны");
+            //    return;
+
+            //    int Sum(int[] numbers)
+            //    {
+            //        int result = 0;
+            //        foreach (int number in numbers)
+            //            result += number;
+            //        return result;
+            //    }
+            //}
+
+
+            //int Sum(int[] numbers)   {
+            //    int limit = 0;
+            //    int result = 0;
+
+
+
+            //    foreach (int number in numbers) {
+            //        if (IsPassed(number, lim:limit)) result += number;
+            //    }
+
+            //    return result;
+            //    static bool IsPassed(int number, int lim) {
+            //        return number > lim;
+            //        bool IsPassed(int number) {
+            //        return number > limit;
+            //    }
+
+
+            //}
+
+            //}
+            //int[] numbers1 = [-3, -2, -1, 0, 1, 2, 3];
+            //int[] numbers2 = [3, -4, 5, -6, 7];
+            //Console.WriteLine(Sum(numbers1));
+            //Console.WriteLine(Sum(numbers2));
+
+
+
+            //Задание 1
+
+            //1.3
+            //2.2
+            //3.2
+            //4.2
+            //5.2
+
+            //Задание 2
+
+            //void GreetUser(string name)
+            //{
+            //    Console.WriteLine($"Привет, {name}! Добро пожаловать в программу");
+            //}
+            //GreetUser("Данил");
+
+            //Задание 3
+
+            //void number1(int num1,int num2)
+            //{
+            //    int result = num1 + num2;
+            //    Console.WriteLine($"{num1} + {num2} = {result}");
+            //}
+            //number1(2, 2);
+
+            //Задание 4
+
+            // bool IsEven (int a)
+            //{
+            //    return a % 2 == 0;
+            //}
+            //Console.WriteLine(IsEven(5)); 
+
+            //Задание 5
+
+            //int[] nums = { 1, 2, 3, 4, 5 };
+            //int result = FindMax(nums);
+            //Console.WriteLine(result);
+
+            //int FindMax(int[] numbers)
+            //{
+
+            //    int max = numbers[0];
+
+            //    for (int i = 0; i < numbers.Length; i++)
+            //    {
+            //        if (numbers[i] > max)
+            //        {
+            //            max = numbers[i];
+            //        }
+            //    }
+            //    return max;
+
+            //}
+
+            //Задание 6
+
+            //Console.WriteLine();
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //int num = 0;
+            //int temp = ConvertCelsiusToFahrenheit(num);
+            //Console.WriteLine(temp);
+
+            //int ConvertCelsiusToFahrenheit (int a)
+            //{
+            //    return a * 9 / 5 + 32;
+            //}
+
+            //Задание 7
+
+            //string text = "Hello World";
+            //int result = CountVowels(text);
+            //Console.WriteLine(result);
+
+            //int CountVowels(string input)
+            //{
+            //    string vowels = "aeiouAEIOU";
+            //    int count = 0;
+
+            //    foreach (char i in input)
+            //    {
+            //        if (vowels.Contains(i))
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    return count;
+            //}
+
+
+            //Задание 8
+
+            //int length = 10;
+            //string password = GeneratePassword(length);
+            //Console.WriteLine(password);
+
+            //static string GeneratePassword(int length)
+            //{
+
+            //    string text = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            //    var random = new Random();
+            //    var result = new char[length];
+
+            //    for (int i = 0; i < length; i++)
+            //    {
+            //        result[i] = text[random.Next(text.Length)];
+            //    }
+            //    return new string(result);
+            //}
+
+
+            //Задание 10
+
+        
+            void ReverxeArray(string[]words)
+            {
+                foreach (string i in words)
+                { 
+                
+                }
+            }
+
+
+            //Задание 11
+
+            //Console.WriteLine("Введите номер операции (1, 2, 3, 4)");
+            //int operation = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите первое число");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите второе число");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //void operation1 (int num1, int num2)
+            //{
+            //    Console.WriteLine(num1 + num2);
+            //}
+            //void operation2 (int num1, int num2)
+            //{
+            //    Console.WriteLine(num1 - num2);
+            //}
+            //void operation3 (int num1, int num2)
+            //{
+            //    Console.WriteLine(num1 * num2);
+            //}
+            //void operation4 (int num1, int num2)
+            //{
+            //    Console.WriteLine(num2 / num2);
+            //}
+
+            //switch (operation)
+            //{
+            //    case 1: operation1(num1, num2); break;
+            //    case 2: operation2(num1, num2); break;
+            //    case 3: operation3(num1, num2); break;
+            //    case 4: operation4(num1, num2); break;
+            //    default: Console.WriteLine("Такой операции нет");break;
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
---
+++ Lab6/Lab6.cs
@@ -0,0 +1,302 @@
+﻿namespace Lab_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    int x = 5;
+            //    int y = x / 0;
+            //    Console.WriteLine($"Результат: {y}");
+            //}
+            //catch (DivideByZeroException ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine($"Error: {ex.Message}");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Блок finally");
+            //}
+            //Console.WriteLine("Конец программы");
+
+            //Console.WriteLine("Введите первое число: ");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите второе число: ");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введите операцию (+, -, *, .): ");
+            //string? op = Console.ReadLine();
+            //double result = 0;
+
+            //if (op == "+") result = number1 + number2;
+            //else if (op == "-") result = number1 - number2;
+            //else if (op == "*") result = number1 * number2;
+            //else if (op == "/") result = number1 / number2;
+            //else Console.WriteLine("Неизвестная операция");
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        switch (op)
+            //        {
+            //            case "+": result = number1 + number2; break;
+            //            case "-": result = number1 - number2; break;
+            //            case "*": result = number1 * number2; break;
+            //            case "/":
+            //                if (number2 == 0)
+            //                    throw new DivideByZeroException("Нельзя делить на ноль!");
+            //                result = number2 / number1; break;
+            //            default: Console.WriteLine("Неизвестная операция"); return;
+            //        }
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("Ошибка ввода! Введите корректные числа.");
+            //    }
+            //    catch (DivideByZeroException ex)
+            //    {
+            //        Console.WriteLine($"Error: {ex.Message}");
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        Console.WriteLine($"Произошла непревиденная ошибка: {ex.Message}");
+            //    }
+            //}
+
+            //int number = 12;
+            //Console.WriteLine(number.ToString());
+            //bool boolean = true;
+            //Console.WriteLine(boolean.ToString());
+            //DateTime now = DateTime.Now;
+            //Console.WriteLine(now.ToString());
+            //object me = new();
+            //Console.WriteLine(me.ToString());
+
+            //int age = int.Parse("27");
+            //DateTime birthday = DateTime.Parse("4 July 1980");
+
+            //Console.Write("Какой максимальный балл по дисциплинам? ");
+            //string? input = Console.ReadLine();
+            //if (int.TryParse(input, out int count))
+            //{
+            //    Console.WriteLine($"Это {count} баллов.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Некорректный ввод.");
+            //}
+
+            //Console.Write("Введите сумму: ");
+            //string? amount = Console.ReadLine();
+            //try
+            //{
+            //    decimal amountValue = decimal.Parse(amount);
+            //}
+            //catch (FormatException) when (amount.Contains("$"))
+            //{
+            //    Console.WriteLine("В суммах нельзя использовать знак доллара!");
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Суммы должны содержать только цифры!");
+            //}
+
+            //try
+            //{
+            //    checked
+            //    {
+            //        int x = int.MaxValue - 1;
+            //        Console.WriteLine($"Initial value: {x}");
+            //        x++;
+            //        Console.WriteLine($"After incrementing: {x}");
+            //        x++;
+            //        Console.WriteLine($"After incrementing: {x}");
+            //        x++;
+            //        Console.WriteLine($"After incrementing: {x}");
+            //    }
+            //}
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine("The code overflowed but I caught the exception.");
+            //}
+
+            // задание 1
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Введите первое число: ");
+            //    double a = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("Введите второе число: ");
+            //    double b = Convert.ToDouble(Console.ReadLine());
+            //    Console.WriteLine("Введите операцию (+, -, *, /, ^) or 'exit' to stop working: ");
+
+            //    string? op = Console.ReadLine();
+
+            //    if (op == "exit")
+            //        break;
+
+            //    double result = 0;
+
+            //    static double Sum(double a, double b)
+            //    {
+            //        return a + b;
+            //    }
+            //    static double Minus(double a, double b)
+            //    {
+            //        return a - b;
+            //    }
+            //    static double Umn(double a, double b)
+            //    {
+            //        return a * b;
+            //    }
+            //    static double Del(double a, double b)
+            //    {
+            //        if (b == 0)
+            //        {
+            //            return 0;
+            //        }
+            //        return a / b;
+            //    }
+            //    static double Step(double a, double b)
+            //    {
+            //        return Math.Pow(a, b);
+            //    }
+            //    switch (op)
+            //    {
+            //        case "+": result = Sum(a, b); break;
+            //        case "-": result = Minus(a, b); break;
+            //        case "*": result = Umn(a, b); break;
+            //        case "/": result = Del(a, b); break;
+            //        case "^": result = Step(a, b); break;
+            //    }
+            //    Console.WriteLine(result);
+            //}
+
+            // Задание 3 
+
+            //Console.Write("Введите делимое: ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //double b;
+            //double result = 0;
+
+            //while (true)
+            //{
+            //    Console.Write("Введите делитель: ");
+            //    b = Convert.ToDouble(Console.ReadLine());
+
+            //    if (b == 0)
+            //    {
+            //        Console.Write("На ноль делить нельзя. Повторите ввод делителя: ");
+            //    }
+            //    else
+            //    {
+            //        break;
+            //    }
+            //}
+            //result = a / b;
+            //Console.WriteLine($"Результат деления: {result}");
+
+            // задание 4
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        Console.Write("Введите целое число: ");
+            //        int a = int.Parse(Console.ReadLine());
+            //        break;
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("Неверный ввод!");
+            //    }
+            //}
+
+            // задание 5
+
+            while (true)
+            {
+                try
+                {
+                    // ИСПРАВЛЕНИЕ 1: Добавлена обработка FormatException
+                    Console.WriteLine("Введите первое число: ");
+                    double a = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Введите операцию (+, -, *, /, ^) or 'exit' to stop working: ");
+                    string? op = Console.ReadLine();
+
+                    if (op == "exit")
+                        break;
+
+                    double result = 0;
+
+                    static double Sum(double a, double b)
+                    {
+                        return a + b;
+                    }
+                    static double Minus(double a, double b)
+                    {
+                        return a - b;
+                    }
+                    static double Umn(double a, double b)
+                    {
+                        return a * b;
+                    }
+                    static double Del(double a, double b)
+                    {
+                        // ИСПРАВЛЕНИЕ 2: Правильная проверка деления на ноль
+                        if (b == 0)
+                        {
+                            Console.WriteLine("Ошибка: деление на ноль!");
+                            return 0; // специальное значение для ошибки
+                        }
+                        return a / b;
+                    }
+                    static double Step(double a, double b)
+                    {
+                        // ИСПРАВЛЕНИЕ 3: Исправлено присваивание переменных
+                        return Math.Pow(a, b); // ПРАВИЛЬНО: a в степени b
+                    }
+
+                    bool validOperation = true;
+
+                    switch (op)
+                    {
+                        case "+": result = Sum(a, b); break;
+                        case "-": result = Minus(a, b); break;
+                        case "*": result = Umn(a, b); break;
+                        case "/": result = Del(a, b); break;
+                        case "^": result = Step(a, b); break;
+                        default:
+                            Console.WriteLine("Неизвестная операция!");
+                            validOperation = false;
+                            break;
+                    }
+
+                    if (validOperation)
+                        Console.WriteLine($"Результат: {result}");
+                }
+                catch (FormatException)
+                {
+                    // ИСПРАВЛЕНИЕ 1: Обработка неверного формата ввода.
+                    Console.WriteLine("Ошибка! Введите корректное число.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла непредвиденная ошибка: {ex.Message}");
+                }
+            }
+        }
+    }
+}
